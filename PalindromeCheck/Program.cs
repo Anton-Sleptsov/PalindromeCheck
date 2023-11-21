9e4f24ea4ba175b2457b1a9cc7830238d6c06ef8
Anton-Sleptsov <arslepcov@kait20.ru>
2023-11-21 21:22:10 +0300

    Добавьте файлы проекта.

---
+++ PalindromeCheck/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace PalindromeCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            if(IsPalindrome(str))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Это палиндром");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Это не палиндром");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        static bool IsPalindrome(string strStart)
+        {
+            string strEnd;
+
+            strStart = ReplaceSymbols(strStart);
+            strStart = strStart.ToLower(); 
+            
+            strEnd = Reverse(strStart);
+
+            if (strStart ==  strEnd)
+                return true;
+            else
+                return false;
+        }
+
+        static string ReplaceSymbols(string str)
+        {    
+            return str.Replace(" ", "").Replace(",", "").Replace(".", "").Replace("!", "").Replace("?", "").Replace("-", "").Replace("\"", "");
+        }
+
+        static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
